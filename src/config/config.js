3a21aafd0f27351f24bdd32c55e6d18481741835
slowly02 <slowlyhg960327@163.com>
2020-06-03 09:49:55 +0800

    third

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
     crossDomain: false, //是否开启跨域支持
-    PROD_URL: 'http://example.com/index.php', //生产环境接口地址
+    PROD_URL: 'http://lvshioa_api.site888.cn/', //生产环境接口地址
     WS_URI: 'ws://192.168.31.68:2345',//wss://beta.vilson.xyz:2345 //WebSocket地址
     HOME_PAGE: '/home',//主页路由
 };
