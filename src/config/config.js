266a05535b7d6344b003a3fc496a950d79509390
slowly02 <slowlyhg960327@163.com>
2020-06-03 10:34:21 +0800

    e

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
-    crossDomain: true, //是否开启跨域支持
+    crossDomain: false, //是否开启跨域支持
     PROD_URL: 'http://lvshioa_api.site888.cn/', //生产环境接口地址
-    WS_URI: 'wss://beta.vilson.xyz:2345',//wss://beta.vilson.xyz:2345 //WebSocket地址ws://192.168.31.68:2345
+    WS_URI: 'ws://192.168.31.68:2345',//wss://beta.vilson.xyz:2345 //WebSocket地址ws://192.168.31.68:2345
     HOME_PAGE: '/home',//主页路由
 };
